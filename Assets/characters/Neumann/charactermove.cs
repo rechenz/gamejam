b94c24bb6927d2d731a06f0a12b40d80a852a88b
rechenz <13244571368@qq.com>
2026-02-07 00:32:57 +0800

    增加主角动画

--- Assets/characters/Neumann/charactermove.cs
+++ Assets/characters/Neumann/charactermove.cs
@@ -7,12 +7,13 @@ public class charactermove : MonoBehaviour
     public float xspeed = 5f;
     public float yspeed = 5f;
     public bool isspace = false;
-
+    Animator animator;
     Transform Tr;
     // Start is called before the first frame update
     void Start()
     {
         Tr = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
     }
 
     void moveleft()
@@ -34,16 +35,31 @@ public class charactermove : MonoBehaviour
     {
         Tr.Translate(0, -yspeed * Time.deltaTime, 0);
     }
+
+    void clear()
+    {
+        animator.SetBool("isstay", false);
+        animator.SetBool("moveleft", false);
+        animator.SetBool("moveright", false);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        clear();
         if (Input.GetKey(KeyCode.A))
         {
+            animator.SetBool("moveleft", true);
             moveleft();
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
+            animator.SetBool("moveright", true);
             moveright();
+        }
+        else
+        {
+            animator.SetBool("isstay", true);
         }
         if (isspace)
         {
