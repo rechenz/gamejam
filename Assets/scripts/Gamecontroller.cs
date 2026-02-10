78b740e2321d5d3597e7c6606782a1a96fbebee2
GEORGEWU <q2983150050@gmail.com>
2026-02-10 12:21:00 +0800

    GameUI.cs变成纯显示层+单例，游戏逻辑移到GameController.cs，Point和Shield分场景使用

--- Assets/scripts/Gamecontroller.cs
+++ Assets/scripts/Gamecontroller.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Game.UI;
 
 public class Gamecontroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("玩家属性")]
+    [SerializeField] private int maxHP = 100;
+    [SerializeField] private int maxShield = 50;
+    
+    private int currentHP;
+    private int currentShield;
+
     void Start()
     {
-        
+        currentHP = maxHP;
+        currentShield = maxShield;
+        UpdateHPUI();
+        UpdateShieldUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
+        if (currentShield > 0)
+        {
+            int shieldDamage = Mathf.Min(currentShield, damage);
+            currentShield -= shieldDamage;
+            damage -= shieldDamage;
+        }
         
+        if (damage > 0)
+        {
+            currentHP = Mathf.Max(currentHP - damage, 0);
+        }
+        
+        UpdateHPUI();
+        UpdateShieldUI();
+        
+        if (currentHP <= 0) Die();
+    }
+
+    public void HealHP(int amount)
+    {
+        currentHP = Mathf.Min(currentHP + amount, maxHP);
+        UpdateHPUI();
+    }
+
+    public void HealShield(int amount)
+    {
+        currentShield = Mathf.Min(currentShield + amount, maxShield);
+        UpdateShieldUI();
+    }
+
+    private void UpdateHPUI()
+    {
+        if (GameUI.Instance != null)
+            GameUI.Instance.SetHP(currentHP, maxHP);
+    }
+
+    private void UpdateShieldUI()
+    {
+        if (GameUI.Instance != null)
+            GameUI.Instance.SetShield(currentShield, maxShield);
+    }
+
+    private void Die()
+    {
+        Debug.Log("玩家死亡");
     }
 }
